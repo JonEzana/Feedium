d710879582ad4340466e60e2ada06f8dca8f5787
Jon <jon.ezana1@gmail.com>
2023-09-07 14:22:35 -0400

    add prod prefixes for join table columns

--- app/models/story_topic.py
+++ app/models/story_topic.py
@@ -1,9 +1,15 @@
-from .db import db
+from .db import db, add_prefix_for_prod
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 
 story_topics = db.Table(
     "story_topics",
     db.Model.metadata,
-    db.Column("story_id", db.Integer, db.ForeignKey("stories.id"), primary_key=True),
-    db.Column("topic_id", db.Integer, db.ForeignKey("topics.id"), primary_key=True)
+    db.Column("story_id", db.Integer, db.ForeignKey(add_prefix_for_prod("stories.id")), primary_key=True),
+    db.Column("topic_id", db.Integer, db.ForeignKey(add_prefix_for_prod("topics.id")), primary_key=True)
 )
+
+if environment == "production":
+    story_topics.schema = SCHEMA
