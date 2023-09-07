d6143ed78bee1e0632c7104eac3ea5143b3848d9
Jon <jon.ezana1@gmail.com>
2023-09-07 14:04:56 -0400

    create story and display story working locally

--- react-app/src/store/stories.js
+++ react-app/src/store/stories.js
@@ -1,8 +1,26 @@
-const GET_MOST_POPULAR_STORIES = "stories/getMostPopular"
+const GET_MOST_POPULAR_STORIES = "stories/getMostPopular";
+const GET_CURRENT_USER_STORIES = "stories/getCurrentUser";
+const GET_SINGLE_STORY = "stories/getSingle";
+const CREATE_STORY = "stories/create";
 
 const getMostPopularStories = (stories) => ({
     type: GET_MOST_POPULAR_STORIES,
     payload: stories
+});
+
+const getCurrentUserStories = (stories) => ({
+    type: GET_CURRENT_USER_STORIES,
+    payload: stories
+});
+
+const getSingleStory = (story) => ({
+    type: GET_SINGLE_STORY,
+    payload: story
+});
+
+const createStory = (story) => ({
+    type: CREATE_STORY,
+    payload: story
 });
 
 export const thunkGetMostPopularStories = () => async (dispatch) => {
@@ -16,7 +34,47 @@ export const thunkGetMostPopularStories = () => async (dispatch) => {
     }
 }
 
-const initialState = {allStories: {}, hotStories: {}, usersStories: {}}
+export const thunkGetCurrentUserStories = (userId) => async (dispatch) => {
+    const res = await fetch(`/api/stories/users/${userId}/stories`);
+    if (res.ok) {
+        const stories = await res.json();
+        dispatch(getCurrentUserStories(stories.stories))
+        return stories
+    } else {
+        console.log('current user storoes fetch failed');
+    }
+}
+
+export const thunkGetSingleStory = (storyId) => async (dispatch) => {
+    const res  = await fetch(`/api/stories/${storyId}`);
+    if (res.ok) {
+        const story = await res.json();
+        dispatch(getSingleStory(story));
+        return story;
+    }
+}
+
+export const thunkCreateStory = (story) => async (dispatch) => {
+    const res = await fetch('/api/stories/new', {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(story)
+    });
+    if (res.ok) {
+        const newStory = await res.json();
+        dispatch(createStory(newStory));
+        return newStory;
+    } else if (res.status < 500) {
+		const data = await res.json();
+		if (data.errors) {
+			return data.errors;
+		}
+	} else {
+		return ["An error occurred. Please try again."];
+	}
+}
+
+const initialState = {allStories: {}, hotStories: {}, usersStories: {}, singleStory: {}}
 
 export default function storiesReducer(state = initialState, action) {
     switch (action.type) {
@@ -26,6 +84,30 @@ export default function storiesReducer(state = initialState, action) {
                 newState.hotStories[story.id] = story
             });
             return newState;
+        }
+        case GET_CURRENT_USER_STORIES: {
+            const newState = {...state, allStories: {...state.allStories}, userStories: {}};
+            action.payload.forEach(story => {
+                newState.userStories[story.id] = story;
+            });
+            return newState
+        }
+        case GET_SINGLE_STORY: {
+            return {
+                ...state,
+                allStories: {...state.allStories, [action.payload.id]: action.payload},
+                hotStories: {...state.hotStories},
+                userStories: {...state.usersStories},
+                singleStory: {...action.payload}
+            }
+        }
+        case CREATE_STORY: {
+            return {
+                ...state,
+                allStories: {...state.allStories, [action.payload.id]: action.payload},
+                userStories: {...state.usersStories, [action.payload.id]: action.payload},
+                singleStory: {...action.payload}
+            }
         }
         default:
             return state;
