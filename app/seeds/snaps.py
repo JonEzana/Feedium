d5542e5651c2212002145f9806d30df739449831
Jon <jon.ezana1@gmail.com>
2023-10-09 17:26:46 -0400

    seed all snaps

--- app/seeds/snaps.py
+++ app/seeds/snaps.py
@@ -15,10 +15,10 @@ def seed_snaps():
     # all_stories = [story.to_dict() for story in stories]
     # print('~~~~~ SNAP SEEDER: ALL STORIES~~~~~~~~', all_stories)
     # print('~~~~~ SNAP SEEDER: ALL USERS~~~~~~~~', all_users)
-    snap1 = Snap(
-        user_id=1,
-        story_id=1
-    )
+    # snap1 = Snap(
+    #     user_id=1,
+    #     story_id=1
+    # )
     # snap2 = Snap(
     #     user_id=1,
     #     story_id=1
@@ -28,86 +28,86 @@ def seed_snaps():
     #     story_id=3
     # )
 
-    # snap1 = Snap(
-    #     user_id=random.randint(1, 4),
-    #     story_id=random.randint(1, 20)
-    # )
-    # snap2 = Snap(
-    #     user_id=random.randint(1, 4),
-    #     story_id=random.randint(1, 20)
-    # )
-    # snap3 = Snap(
-    #     user_id=random.randint(1, 4),
-    #     story_id=random.randint(1, 20)
-    # )
-    # snap4 = Snap(
-    #     user_id=random.randint(1, 4),
-    #     story_id=random.randint(1, 20)
-    # )
-    # snap5 = Snap(
-    #     user_id=random.randint(1, 4),
-    #     story_id=random.randint(1, 20)
-    # )
-    # snap6 = Snap(
-    #     user_id=random.randint(1, 4),
-    #     story_id=random.randint(1, 20)
-    # )
-    # snap7 = Snap(
-    #     user_id=random.randint(1, 4),
-    #     story_id=random.randint(1, 20)
-    # )
-    # snap8 = Snap(
-    #     user_id=random.randint(1, 4),
-    #     story_id=random.randint(1, 20)
-    # )
-    # snap9 = Snap(
-    #     user_id=random.randint(1, 4),
-    #     story_id=random.randint(1, 20)
-    # )
-    # snap10 = Snap(
-    #     user_id=random.randint(1, 4),
-    #     story_id=random.randint(1, 20),
-    # )
-    # snap11 = Snap(
-    #     user_id=random.randint(1, 4),
-    #     story_id=random.randint(1, 20),
-    # )
-    # snap12 = Snap(
-    #     user_id=random.randint(1, 4),
-    #     story_id=random.randint(1, 20),
-    # )
-    # snap13 = Snap(
-    #     user_id=random.randint(1, 4),
-    #     story_id=random.randint(1, 20),
-    # )
-    # snap14 = Snap(
-    #     user_id=random.randint(1, 4),
-    #     story_id=random.randint(1, 20),
-    # )
-    # snap15 = Snap(
-    #     user_id=random.randint(1, 4),
-    #     story_id=random.randint(1, 20),
-    # )
-    # snap16 = Snap(
-    #     user_id=random.randint(1, 4),
-    #     story_id=random.randint(1, 20),
-    # )
-    # snap17 = Snap(
-    #     user_id=random.randint(1, 4),
-    #     story_id=random.randint(1, 20),
-    # )
-    # snap18 = Snap(
-    #     user_id=random.randint(1, 4),
-    #     story_id=random.randint(1, 20),
-    # )
-    # snap19 = Snap(
-    #     user_id=random.randint(1, 4),
-    #     story_id=random.randint(1, 20),
-    # )
-    # snap20 = Snap(
-    #     user_id=random.randint(1, 4),
-    #     story_id=random.randint(1, 20),
-    # )
+    snap1 = Snap(
+        user_id=random.randint(1, 4),
+        story_id=random.randint(1, 20)
+    )
+    snap2 = Snap(
+        user_id=random.randint(1, 4),
+        story_id=random.randint(1, 20)
+    )
+    snap3 = Snap(
+        user_id=random.randint(1, 4),
+        story_id=random.randint(1, 20)
+    )
+    snap4 = Snap(
+        user_id=random.randint(1, 4),
+        story_id=random.randint(1, 20)
+    )
+    snap5 = Snap(
+        user_id=random.randint(1, 4),
+        story_id=random.randint(1, 20)
+    )
+    snap6 = Snap(
+        user_id=random.randint(1, 4),
+        story_id=random.randint(1, 20)
+    )
+    snap7 = Snap(
+        user_id=random.randint(1, 4),
+        story_id=random.randint(1, 20)
+    )
+    snap8 = Snap(
+        user_id=random.randint(1, 4),
+        story_id=random.randint(1, 20)
+    )
+    snap9 = Snap(
+        user_id=random.randint(1, 4),
+        story_id=random.randint(1, 20)
+    )
+    snap10 = Snap(
+        user_id=random.randint(1, 4),
+        story_id=random.randint(1, 20),
+    )
+    snap11 = Snap(
+        user_id=random.randint(1, 4),
+        story_id=random.randint(1, 20),
+    )
+    snap12 = Snap(
+        user_id=random.randint(1, 4),
+        story_id=random.randint(1, 20),
+    )
+    snap13 = Snap(
+        user_id=random.randint(1, 4),
+        story_id=random.randint(1, 20),
+    )
+    snap14 = Snap(
+        user_id=random.randint(1, 4),
+        story_id=random.randint(1, 20),
+    )
+    snap15 = Snap(
+        user_id=random.randint(1, 4),
+        story_id=random.randint(1, 20),
+    )
+    snap16 = Snap(
+        user_id=random.randint(1, 4),
+        story_id=random.randint(1, 20),
+    )
+    snap17 = Snap(
+        user_id=random.randint(1, 4),
+        story_id=random.randint(1, 20),
+    )
+    snap18 = Snap(
+        user_id=random.randint(1, 4),
+        story_id=random.randint(1, 20),
+    )
+    snap19 = Snap(
+        user_id=random.randint(1, 4),
+        story_id=random.randint(1, 20),
+    )
+    snap20 = Snap(
+        user_id=random.randint(1, 4),
+        story_id=random.randint(1, 20),
+    )
     # snap1 = Snap(
     #     user_id=1,
     #     story_id=1
@@ -190,7 +190,7 @@ def seed_snaps():
     # )
     db.session.add(snap1)
     # db.session.add_all([snap1, snap2, snap3])
-    # db.session.add_all([snap1, snap2, snap3, snap4, snap5, snap6, snap7, snap8, snap9, snap10, snap11, snap12, snap13, snap14, snap15, snap16, snap17, snap18, snap19, snap20])
+    db.session.add_all([snap1, snap2, snap3, snap4, snap5, snap6, snap7, snap8, snap9, snap10, snap11, snap12, snap13, snap14, snap15, snap16, snap17, snap18, snap19, snap20])
     db.session.commit()
 
 def undo_snaps():
